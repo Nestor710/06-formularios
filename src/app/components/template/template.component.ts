664a86f870bf16713926e27a42d3107829a5a2c7
Nestor <nestorlima710@gmail.com>
2021-01-14 20:20:40 -0500

    act

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: [
-  ]
+  styles: [`
+    .ng-invalid.ng-touched:not(Form) {
+      border: 2px solid red;
+    }
+  `]
 })
 export class TemplateComponent {
 
+  usuario:any = {
+    nombre: null,
+    apellido: null,
+    correo: null
+  }
+
   constructor() { }
 
-  guardar(){
-    console.log('Llamado la función');
+  guardar( forma:NgForm ){
+    console.log('formulario posteado');
+    console.log('ngForm',forma);
+    console.log('Valor', forma.value);
+    console.log('Usuario', this.usuario);
+    
+
+    
     
   }
 
